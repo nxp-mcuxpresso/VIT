1c2ef5dc34b0891cac0633d824740006951b52c7
Pierre Moos <pierre.moos_1@nxp.com>
2023-01-09 15:10:20 +0100

    Init
    Signed-off-by: Pierre Moos <pierre.moos_1@nxp.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+
+NXP's voice intelligent technology (VIT) library provides voice UI enablement with always-on wake word detection and local commands recognition.
---
+++ middleware_vit_cm7.cmake
@@ -0,0 +1,11 @@
+#Description: middleware vit cm7; user_visible: True
+include_guard(GLOBAL)
+message("middleware_vit_cm7 component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+)
+
+
