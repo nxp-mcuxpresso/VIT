764dc87d5d0c74c2e797fb3fc9d46b38b2786932
mcu_sdk.ci <mcu_sdk.ci@nxp.com>
2023-10-31 13:38:24 +0000

    Add files for release/2.13.3_rw612_rfp2

---
+++ middleware_vit_cm33-rw61x.cmake
@@ -0,0 +1,11 @@
+#Description: middleware vit cm33-rw61x; user_visible: True
+include_guard(GLOBAL)
+message("middleware_vit_cm33-rw61x component is included.")
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+)
+
+
